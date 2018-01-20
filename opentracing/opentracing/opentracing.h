6be9c0e4f0eb6729dc23215ffbf41521a65c8e37
jeff <fangyang@yy.com>
2018-01-20 20:45:59 +0800

    初始化工程

---
+++ opentracing/opentracing/opentracing.h
@@ -0,0 +1,19 @@
+//
+//  opentracing.h
+//  opentracing
+//
+//  Created by 方阳 on 2018/1/20.
+//  Copyright © 2018年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for opentracing.
+FOUNDATION_EXPORT double opentracingVersionNumber;
+
+//! Project version string for opentracing.
+FOUNDATION_EXPORT const unsigned char opentracingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <opentracing/PublicHeader.h>
+
+
